c77c7e11b9ea1a97b3d816faa5b2adf1d1e8daa1
francklebas <francklebas@ik.me>
2026-03-24 16:47:44 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxt/devtools",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "reka-ui",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "@nuxtjs/i18n",
+    "@nuxtjs/tailwindcss",
+    "nuxt-security",
+    "@nuxtjs/turnstile",
+    "@nuxt/icon",
+    "nuxt-lucide-icons",
+  ],
+  supabase: {
+    redirect: true,
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+    },
+  },
+});
