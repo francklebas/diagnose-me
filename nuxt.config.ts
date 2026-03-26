a4370aa98b9abd0194501bbb74bcb7c799bb5fb8
francklebas <francklebas@ik.me>
2026-03-26 12:28:45 +0100

    fix(deploy): add cloudflare-pages preset for Nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   pages: true,
+  nitro: {
+    preset: "cloudflare-pages",
+  },
   modules: [
     "@nuxtjs/supabase",
     "@nuxt/devtools",
